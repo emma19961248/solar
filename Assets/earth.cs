b95c2b6554fb508c55b07c465399d04e1950c8d4
emma19961248 <gintma7336@gmail.com>
2018-10-07 16:53:18 +0800

    Initial commit

---
+++ Assets/earth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour {
+
+    //public float y;
+
+    // Update is called once per frame
+    void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, 0.8f, 0);
+        transform.Rotate(angle);
+    }
+}
